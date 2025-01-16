937154d5ad3f50bd9a62e6a6bc0a75359f81a737
Atikur Rahman <atikur.ice18@gmail.com>
2025-01-16 23:41:38 +0600

    Exam 2.

--- Inventory_App/Controllers/HomeController.cs
+++ Inventory_App/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Inventory_App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 
 namespace Inventory_App.Controllers
 {
@@ -26,5 +28,45 @@ namespace Inventory_App.Controllers
 
             return View();
         }
+
+        public ActionResult Employee()
+        {
+            // Sample data , We can import data from database
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    EmployeeId = 1,
+                    EmployeeName = "John Doe",
+                    EmployeeAddress = "123 Main St",
+                    EmployeePhone = "555-1234",
+                    EmployeeEmail = "john.doe@example.com",
+                    EmployeePosition = "Developer",
+                    Projects = new List<Project>
+                    {
+                        new Project { ProjectId = 1, ProjectName = "Project A", ProjectDescription = "Description A", ProjectUrl = "http://example.com/a" },
+                        new Project { ProjectId = 2, ProjectName = "Project B", ProjectDescription = "Description B", ProjectUrl = "http://example.com/b" }
+                    }
+                },
+                new Employee
+                {
+                    EmployeeId = 2,
+                    EmployeeName = "Jane Smith",
+                    EmployeeAddress = "456 Elm St",
+                    EmployeePhone = "555-5678",
+                    EmployeeEmail = "jane.smith@example.com",
+                    EmployeePosition = "Manager",
+                    Projects = new List<Project>
+                    {
+                        new Project { ProjectId = 3, ProjectName = "Project C", ProjectDescription = "Description C", ProjectUrl = "http://example.com/c" }
+                    }
+                }
+            };
+
+            ViewBag.Employees = employees;
+
+            return View("EmployeeView");
+        }
     }
+
 }
---
+++ Inventory_App/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory_App.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public string EmployeePhone { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string EmployeePosition { get; set; }   
+        public List<Project> Projects { get; set; }
+
+    }
+
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectUrl { get; set; }
+    }
+
+}
