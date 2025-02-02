5b94e6b43da335e418ae6947eb0ecf8fb66d15df
Atikur Rahman <atikur.ice18@gmail.com>
2025-02-02 21:45:21 +0600

    Implementation of Classes Code.

--- Inventory_App/App_Start/RouteConfig.cs
+++ Inventory_App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Inventory_App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Inventory", action = "Dashboard", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Inventory_App/Models/BaseCustomer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_App.Models
+{
+    public class BaseCustomer
+    {
+        public static DataTable ListCustomer()
+        {
+            DataTable dataTable1 = new DataTable();
+            string ConnString = ConfigurationManager.ConnectionStrings["ConnString"].ToString();
+            SqlConnection sqlConnection = new SqlConnection(ConnString);
+
+            try
+            {
+                sqlConnection.Open();
+                string Query = "spOST_LstCustomer";
+                SqlCommand cmd = new SqlCommand(Query, sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                cmd.CommandTimeout = 0;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);    //fetch mode table data  
+                adapter.Fill(dataTable1);
+                cmd.Dispose();
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                sqlConnection.Close();
+            }
+            return dataTable1;
+        }
+    }
+}
